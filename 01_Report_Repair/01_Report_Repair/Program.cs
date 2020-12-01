1f25e76e106ee8acdb2b45cc31a007b329008b7c
Wolfgang Mayr <wolfgangmayr798@gmail.com>
2020-12-01 10:41:31 +0100

    Added Day 1

---
+++ 01_Report_Repair/01_Report_Repair/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace _01_Report_Repair
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = File.ReadAllLines("input.txt").Select(int.Parse).ToArray();
+
+            //for (var i = 0; i < input.Length; i++)
+            //{
+            //    var numA = input[i];
+            //    for (var j = i + 1; j < input.Length; j++)
+            //    {
+            //        var numB = input[j];
+
+            //        if (numA + numB == 2020)
+            //        {
+            //            Console.WriteLine($"Found numbers {numA} and {numB}: {numA * numB}");
+            //        }
+            //    }
+            //}
+
+            for (var i = 0; i < input.Length; i++)
+            {
+                var numA = input[i];
+                for (var j = i + 1; j < input.Length; j++)
+                {
+                    var numB = input[j];
+
+                    for (var k = j + 1; k < input.Length; k ++)
+                    {
+                        var numC = input[k];
+                        if (numA + numB + numC == 2020)
+                        {
+                            Console.WriteLine($"Found numbers {numA}, {numB} and {numC}: {numA * numB * numC}");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
